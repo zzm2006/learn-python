619973707801d6ccb413559cfc80ab4576951fdd
zzm2006 <3141917969@qq.com>
2023-07-03 20:32:48 +0800

    2023.7.3

---
+++ 7.3.py
@@ -0,0 +1,57 @@
+"""注释
+angel = 50
+print("当前钱包余额:", angel)
+print("购买了冰淇淋", "花费:", 10, "元")
+print("购买了可乐", "花费:", 10, "元")
+angel=angel-10-5
+print("最终", "钱包剩余:", angel, "元")
+"""
+"""注释
+angel="你的胰脏"
+num="我想吃掉：%s" % angel
+print(num)
+"""
+"""注释
+angel="两年半"
+zxc="蔡徐坤"
+angel1="唱跳"
+angel2="rua"
+angel3="篮球"
+num="我是练习时常%s的个人练习生%s, 喜欢%s, %s, %s," %(angel, zxc, angel1, angel2, angel3)
+print(num)
+"""
+"""注释
+angel = "吃了吗"
+angel2 = "吃了好吃的"
+print(f"{angel}, {angel2}")
+"""
+"""注释
+name = "传智播客"
+angel1 = 19.99
+angel5 = 7
+angel6 = "003032"
+angel3 = 1.2
+angel2 = angel1*angel3**angel5
+print(f"公司：{name}, 股票代码:{angel6}, 当前股价:{angel1}")
+num = ("每日增长系数是:%.1f, 经过%s天的增长后, 股价达到了:%.2f" %(angel3,angel5,angel2))
+print(num)
+"""
+"""注释
+angel1 = "两年半"
+angle2 = "蔡徐坤"
+angel3 = "蔡徐坤的粉丝"
+angel4 = "小黑子"
+angel = input("请告诉我， 你是谁？")
+print("我知晓了, 你就是练习时常%s的个人练习生%s" %(angel1, angle2))
+umn = input("请告诉我你的银行卡密码：")
+print("我的银行卡密码是：, 唱跳, rua, 篮球")
+print("您好%s, 您是尊贵的%s用户, 欢迎您的光临, 我们的口号是:, 唱跳, rua, 篮球, 只因你太美！" %(angel3, angel4))
+"""
+
+
+
+
+
+
+
+
