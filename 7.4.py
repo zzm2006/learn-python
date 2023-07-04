b0e6d6f34324c469b67a5a896f1d87d07d6ecc43
zzm2006 <3141917969@qq.com>
2023-07-04 13:16:06 +0800

    2023.7.4（猜数游戏）

---
+++ 7.4.py
@@ -0,0 +1,24 @@
+import random
+angel = random.randint(1, 10)
+you_angel = int(input("请猜一猜数字是多少: "))
+if you_angel == angel:
+    print("恭喜你第一次就猜对了")
+else:
+    if you_angel > angel:
+        print("你猜测的数字大了")
+    else:
+        print("你猜测的数字小了")
+    you_angel= int(input("请再猜一猜数字是多少: "))
+    if you_angel == angel:
+            print("恭喜你第二次猜对了")
+    else:
+        if you_angel>angel:
+                print("你猜测的数字大了")
+        else:
+                print("你猜测的数字小了")    
+        you_angel = int(input("请第三次猜一猜数字是多少: "))
+        if you_angel == angel:
+                print("恭喜你第三次再对了")
+        else:
+                print("三次都猜不对, 你个垃圾")    
+                     
